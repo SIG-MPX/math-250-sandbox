2ab7e07b51902e182edee50d6ae78713e0ee099c
Mark Esquivel <esframark@icloud.com>
2026-06-24 09:56:06 -0500

    initial commit

---
+++ in class june 24.R
@@ -0,0 +1,12 @@
+# class notes 6.24
+
+library(usethis)
+library(tidyverse)
+
+create_github_token() #got token from github
+
+gitcreds::gitcreds_set()
+# Need to install gitcreds package first
+# your creds will be different, duhhhhhhhhhhhhhhhhhhhhhhhhh
+
+#next, save and commit
